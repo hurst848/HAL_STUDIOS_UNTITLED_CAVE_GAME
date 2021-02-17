2b8cc62a2851d363faaf5c73ce3ac519f0771594
hurst848 <61146110+hurst848@users.noreply.github.com>
2021-02-17 12:29:47 +0000

    partially complete light sensors

---
+++ Assets/scripts/AreaLightEmmision.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaLightEmmision : MonoBehaviour
+{
+    public float range;
+    public bool lightOn = false;
+    
+
+    private Light _emmision;
+    private SphereCollider _collider; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _emmision = GetComponent<Light>();
+        _collider = GetComponent<SphereCollider>();
+        _collider.radius = range;
+        _emmision.range = range;
+    }
+
+    public void switchOn() { lightOn = true; _collider.enabled = true; }
+    public void switchOff() { lightOn = false; _collider.enabled = false; }
+}
---
+++ Assets/scripts/spotLightEmmision.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spotLightEmmision : MonoBehaviour
+{
+    public float range;
+    public float spotAngle;
+    public bool lightOn = false;
+    public GameObject colliders;
+    private SphereCollider _endCollider;
+    private Light _light;
+    private float _radius;
+    
+    void Start()
+    {
+        _endCollider = colliders.GetComponent<SphereCollider>();
+        _light = GetComponent<Light>();
+
+        _light.range = range;
+        _light.spotAngle = spotAngle;
+
+        calcualteRadius();
+        colliders.transform.position = new Vector3(
+            transform.localPosition.x,
+            transform.localPosition.y,
+            transform.localPosition.z + range);   
+    }
+
+    public void switchOn()  { lightOn = true;  }
+    public void switchOff() { lightOn = false; }
+
+    private void calcualteRadius()
+    { 
+        _endCollider.radius = (Mathf.Atan(Mathf.Deg2Rad * _light.spotAngle) * range)/1.75f;
+    }
+}
