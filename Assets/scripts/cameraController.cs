e0631a14b378a76be4c3083005b17ef4d9ec4944
AbsolutePrune <78572192+AbsolutePrune@users.noreply.github.com>
2021-02-19 15:43:42 +0000

    added player movement

--- Assets/scripts/cameraController.cs
+++ Assets/scripts/cameraController.cs
@@ -2,42 +2,80 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class playerController : MonoBehaviour
+public class cameraController : MonoBehaviour
 {
 
     public GameObject playerCamera;
 
     [SerializeField]
-    private float walkSpeed, runSpeed, jumpForce, mouseSensitivity, playerGravity;
+    private float walkSpeed, runSpeed, jumpForce, mouseSensitivity, playerGravity, playerHeight;
 
     private Rigidbody _rigidbody;
 
     // private movement varibles
     private bool canMove = true;
     private bool canJump = true;
-    private bool isSprinting = false;
     private bool canSprint = true;
+    private bool canCrouch = true;
+    private bool isSprinting = false;
+    private bool isCrouching = false;
+
+
 
 
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
+       
     }
 
     void Update()
     {
-        // Check if shift is being held
+        // Check if sprint is available
         if (canSprint)
         {
+            // Check if shift is being held
             if (Input.GetKeyDown(KeyCode.LeftShift))
             {
                 isSprinting = true;
+                Debug.Log("holding sprint");
+
+                if(Input.GetKeyUp(KeyCode.LeftShift))
+                {
+                    isSprinting = false;
+                    Debug.Log("let got of sprint");
+                }
             }
             else
             {
                 isSprinting = false;
             }
+
         }
+
+        // Check if crouch is available
+        if (canCrouch)
+        {
+            // Check if control is being held
+            if (Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                isCrouching = true;
+                Debug.Log("holding crouch");
+
+                if (Input.GetKeyUp(KeyCode.LeftControl))
+                {
+                    Debug.Log("let got of crouch");
+                    isCrouching = false;
+                    
+                }
+            }
+            else
+            {
+                isCrouching = false;
+            }
+
+        }
+          
 
         // Get mouse Input from player
         float horizontalAxis = Input.GetAxis("Mouse X") * mouseSensitivity;
@@ -63,6 +101,9 @@ public class playerController : MonoBehaviour
         {
             plyrDir *= walkSpeed;
         }
+
+        // Move the player       
         _rigidbody.velocity = transform.TransformDirection(plyrDir);
+        
     }
 }
