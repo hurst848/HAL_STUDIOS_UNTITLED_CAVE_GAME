dade62c4f9fc52943c6f44c8aad558f73e4a845c
hurst848 <61146110+hurst848@users.noreply.github.com>
2021-03-03 10:23:50 +0000

    Require Room Assets for further development

--- Assets/scripts/monsterScripts/monsterPathfinding.cs
+++ Assets/scripts/monsterScripts/monsterPathfinding.cs
@@ -12,6 +12,6 @@ public class monsterPathfinding : MonoBehaviour
 
     private List<Vector2Int> path;
 
-
+    
     
 }
---
+++ Assets/scripts/runtime/roomController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class roomController : MonoBehaviour
+{
+
+    public List<GameObject> rooms;
+    public int numRooms;
+
+
+    private roomClassifier pathClassifier;
+    private uint seed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        seed = (uint)(Random.Range(0, int.MaxValue));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void generateMap()
+    {
+
+    }
+
+
+    private int interopalateRoomFromNoise()
+    {
+        int roomIndex = 0;
+
+
+        return roomIndex;
+    }
+}
