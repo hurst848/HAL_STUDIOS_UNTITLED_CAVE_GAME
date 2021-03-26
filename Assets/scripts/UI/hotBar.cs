1bc9b26621a52febf69719ee1ff0a6d85b72611a
AbsolutePrune <78572192+AbsolutePrune@users.noreply.github.com>
2021-03-26 15:06:48 +0000

    hotbar done, working on pickup now

--- Assets/scripts/UI/hotBar.cs
+++ Assets/scripts/UI/hotBar.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class hotBar : MonoBehaviour
 {
 
-    private bool changeSprite;
+    //private bool changeSprite;
 
     public Image image1;
     public Image image2;
@@ -15,41 +15,33 @@ public class hotBar : MonoBehaviour
     public Image image5;
 
     public Sprite Knob;
+    public Sprite checkMark;
 
-    /*public void changeImage(string newImageTitle)
-    {
-        image1.sprite = Resources.Load<Sprite>("newImageTitle");
-    }*/
 
     // Start is called before the first frame update
     void Start()
     {
-        changeSprite = false;
+        image1.sprite = Knob;
+        image1.GetComponent<Image>().color = new Color(0, 0, 0, 0);
 
-        image1.GetComponent<Image>().color = new Color (0, 0, 0, 255);
-        //image1.GetComponent<Image>().sprite = Knob;
-        image2.GetComponent<Image>().color = new Color (0, 0, 0, 0);
-        image3.GetComponent<Image>().color = new Color (0, 0, 0, 0);
-        image4.GetComponent<Image>().color = new Color (0, 0, 0, 0);
-        image5.GetComponent<Image>().color = new Color (0, 0, 0, 0);
+        image2.sprite = Knob;
+        image2.GetComponent<Image>().color = new Color(0, 0, 0, 0);
+
+        image3.sprite = Knob;
+        image3.GetComponent<Image>().color = new Color(0, 0, 0, 0);
+
+        image4.sprite = Knob;
+        image4.GetComponent<Image>().color = new Color(0, 0, 0, 0);
+
+        image5.sprite = Knob;
+        image5.GetComponent<Image>().color = new Color(0, 0, 0, 0);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            changeSprite = true;
-        }
+        
     }
 
-    void itemStatus()
-    {
-        Sprite Knob = Resoucres.Load<Sprite>("Knob");
-
-        if(changeSprite)
-        {
-            gameObject.GetComponent<Image>().sprite = Knob;
-        }
-    }
 }
