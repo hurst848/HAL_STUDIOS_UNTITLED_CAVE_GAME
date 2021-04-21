15848da2ea46a6210d10bef6992bd19d07348e4f
hurst848 <61146110+hurst848@users.noreply.github.com>
2021-04-21 12:18:41 +0100

    V1 of menus is mostly done, awaiting assets and player for further progress

--- Assets/scripts/runtime/gameHandler.cs
+++ Assets/scripts/runtime/gameHandler.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class gameHandler : MonoBehaviour
@@ -10,7 +11,18 @@ public class gameHandler : MonoBehaviour
     public static int numMonsters;
 
 
+    public Canvas entryCanvas;
+    public Canvas difficultySelectionCanvas;
+    public Canvas customGameConfiguration;
+    public Canvas settingsMenu;
+
+    public Dropdown resolutionSettings;
+
+    public List<Text> customGameSettings;
+
     [HideInInspector] public difficulty diff;
+
+    Resolution[] resolutions;
 
     public enum difficulty
     {
@@ -19,10 +31,35 @@ public class gameHandler : MonoBehaviour
         hard,
         custom
     }
-    
 
+    private void Start()
+    {
+        switchMenuToEntry();
 
-    void playThegame()
+        resolutions = Screen.resolutions;
+        resolutionSettings.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResIndex = 0;
+        for (int i =0; i< resolutions.Length; i++)
+        {
+            string option = resolutions[i].width + " X " + resolutions[i].height;
+            options.Add(option);
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResIndex = i;
+            }
+        }
+
+        resolutionSettings.AddOptions(options);
+        resolutionSettings.value = currentResIndex;
+        resolutionSettings.RefreshShownValue();
+    }
+
+    public void playThegame()
     {
         switch (diff)
         {
@@ -42,7 +79,14 @@ public class gameHandler : MonoBehaviour
                 numMonsters = 16;
                 break;
             case difficulty.custom:
-                // do nothing as values are already assigned
+                gameSeed = customGameSettings[0].text;
+
+                gameMagnitude = int.Parse(customGameSettings[1].text);
+                if (gameMagnitude == 0) { gameMagnitude = 10; }
+                if (gameMagnitude < 0 ) { gameMagnitude *= -1; }
+
+                numMonsters = int.Parse(customGameSettings[2].text);
+                if (numMonsters < 0) { numMonsters *= -1; }
                 break;
             default:
                 break;
@@ -50,5 +94,62 @@ public class gameHandler : MonoBehaviour
         }
         SceneManager.LoadScene(1);
 
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+    public void setGameEasy() { diff = difficulty.easy; }
+    public void setGameNormal() { diff = difficulty.normal; }
+    public void setGameHard() { diff = difficulty.hard; }
+    public void setGameCustom() { diff = difficulty.custom; switchMenuToCustomConfig(); }
+
+    public void switchMenuToEntry()
+    {
+        entryCanvas.enabled = true;
+        difficultySelectionCanvas.enabled = false;
+        customGameConfiguration.enabled = false;
+        settingsMenu.enabled = false;
+    }
+    public void switchMenuToDiffSelection()
+    {
+        entryCanvas.enabled = false;
+        difficultySelectionCanvas.enabled = true;
+        customGameConfiguration.enabled = false;
+        settingsMenu.enabled = false;
+    }
+    public void switchMenuToCustomConfig()
+    {
+        entryCanvas.enabled = false;
+        difficultySelectionCanvas.enabled = false;
+        customGameConfiguration.enabled = true;
+        settingsMenu.enabled = false;
+    }
+    public void switchMenuToSettings()
+    {
+        entryCanvas.enabled = false;
+        difficultySelectionCanvas.enabled = false;
+        customGameConfiguration.enabled = false;
+        settingsMenu.enabled = true;
+    }
+
+    public void setQuality(int quality)
+    {
+        QualitySettings.SetQualityLevel(quality);
+    }
+
+    public void setFullScreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+
+    public void setResolution(int resIndex)
+    {
+        Screen.SetResolution(
+            resolutions[resIndex].width, 
+            resolutions[resIndex].height, 
+            Screen.fullScreen);
     }
 }
